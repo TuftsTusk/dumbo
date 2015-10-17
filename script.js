054631a29c32b270923b34df359053275f822ec6
jclaw <jclaw3456@gmail.com>
2015-10-17 12:44:01 -0400

    animation working

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
-	$(".slide_left").click(function() {
+	$(".fixed_field .slide_left").click(function() {
 		console.log("clicked");
-		console.log($(this).offset().left);
-		$(this).css("margin-left", "-=200");
+		$(".fixed_field .slide_element").css("margin-left", "+=200");
+	});
 
+	$(".fixed_field .slide_right").click(function() {
+		console.log("clicked");
+		$(".fixed_field .slide_element").css("margin-left", "-=200");
 	});
 });
