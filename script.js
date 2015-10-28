edd6bb3be4e86ea697984b27692c55a998ef6e03
jclaw <jclaw3456@gmail.com>
2015-10-28 19:39:09 -0400

    working animation in extras.html

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 $(document).ready(function() {
 	$(".fixed_field .slide_left").click(function() {
-		console.log("clicked");
 		$(".fixed_field .slide_element").css("margin-left", "+=200");
 	});
 
 	$(".fixed_field .slide_right").click(function() {
-		console.log("clicked");
 		$(".fixed_field .slide_element").css("margin-left", "-=200");
 	});
 });
