16a9d4a4502b05ca8c44e0c44d541d6227de9c51
jclaw <jclaw3456@gmail.com>
2015-11-11 12:17:51 -0500

    worked on extras file

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 $(document).ready(function() {
 	$(".fixed_field .slide_left").click(function() {
-		$(".fixed_field .slide_element").css("margin-left", "+=200");
+		var width = $(".slide_element > li").width();
+		console.log(width)
+		$(".fixed_field .slide_element").css("margin-left", "+=" + width);
 	});
 
 	$(".fixed_field .slide_right").click(function() {
-		$(".fixed_field .slide_element").css("margin-left", "-=200");
+		var width = $(".slide_element > li").width();
+		console.log(width)
+		$(".fixed_field .slide_element").css("margin-left", "-=" + width);
 	});
 });
