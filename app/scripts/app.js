ffff10d501d6f2db49d70ed160d4144b3e930ba2
dcalla02 <dancallahan05@gmail.com>
2015-11-11 15:13:34 -0500

    listings file added

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,7 +9,7 @@
  * Main module of the application.
  */
 angular
-  .module('dumboApp', [
+    .module('dumboApp', [
     'ngAnimate',
     'ngAria',
     'ngCookies',
@@ -19,24 +19,29 @@ angular
     'ngSanitize',
     'ngTouch'
   ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/create_post', {
-        templateUrl: 'views/create_post.html',
-        controller: 'CreatePostCtrl',
-        controllerAs: 'createPost'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl',
+                controllerAs: 'main'
+            })
+            .when('/create_post', {
+                templateUrl: 'views/create_post.html',
+                controller: 'CreatePostCtrl',
+                controllerAs: 'createPost'
+            })
+            .when('/about', {
+                templateUrl: 'views/about.html',
+                controller: 'AboutCtrl',
+                controllerAs: 'about'
+            })
+            .when('/listings', {
+                templateUrl: 'views/listings.html',
+                controller: 'ListingsCtrl',
+                controllerAs: 'listings'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
---
+++ app/scripts/controllers/listings.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dumboApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the dumboApp
+ */
+angular.module('dumboApp')
+  .controller('ListingsCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
