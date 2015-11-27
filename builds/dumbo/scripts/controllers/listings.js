982d13a77e5fe7b714346943b04e4e65ca8f8009
jclaw <jclaw3456@gmail.com>
2015-11-27 00:12:22 -0500

    starting: including semester tray in listings page

--- builds/dumbo/scripts/controllers/listings.js
+++ builds/dumbo/scripts/controllers/listings.js
@@ -8,10 +8,35 @@
  * Controller of the dumboApp
  */
 angular.module('dumboApp')
-  .controller('ListingsCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('ListingsCtrl', function ($scope) {
+  	$scope.semesters = ["Spring 2015", "Summer 2015", "Fall 2015"];
+  	var semIndex = 0;
+  	
+  	var can_move = function(direction, index) {
+  		if (direction == 'left') return index > 0;
+  		if (direction == 'right') return index < $scope.semesters.length - 1;
+  	}
+
+	$(document).ready(function(semIndex) {
+		$(".fixed_field .slide_left").click(function(semIndex) {
+			console.log(semIndex);
+			if (can_move('left', semIndex)) {
+				semIndex++;
+				var width = $(".slide_element > li").width();
+				console.log(width);
+				$(".fixed_field .slide_element").css("margin-left", "+=" + width);
+			}
+			
+		});
+
+		$(".fixed_field .slide_right").click(function() {
+			console.log(semIndex);
+			if (can_move('right', semIndex)) {
+				semIndex++;
+				var width = $(".slide_element > li").width();
+				console.log(width);
+				$(".fixed_field .slide_element").css("margin-left", "-=" + width);
+			}
+		});
+	});
   });
