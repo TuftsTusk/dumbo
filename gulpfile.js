a89d64ef195e22d935c243d9459330c080fb5179
Michael Seltzer <michael.m.seltzer@gmail.com>
2015-12-22 13:09:02 -0700

    run http and https servers on separate ports

--- gulpfile.js
+++ gulpfile.js
@@ -72,13 +72,15 @@ gulp.task('production','', function(){
     key: fs.readFileSync('./ssl/server.key'),
     cert: fs.readFileSync('./ssl/tuskmarketplace_com.crt'),
   };
-  var port = process.env.PORT || 8080;
+  var port = process.env.PORT || 80;
+  var httpsPort = process.env.HTTPSPORT || 443;
   var express = require('express');
   var app = express();
   app.use(express.static('welcome'));
-  var server = https.createServer(options, app).listen(port, function(){
+  var server = https.createServer(options, app).listen(httpsPort, function(){
     console.log("Express server listening on port " + port);
   });
+  app.listen(port);
 });
 
 gulp.task('default', ['sass', 'watch', 'serve']);
