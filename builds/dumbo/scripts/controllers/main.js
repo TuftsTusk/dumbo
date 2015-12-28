ed52c6dff30b1b89050210703f3bef920580e581
jclaw <jclaw3456@gmail.com>
2015-12-28 14:48:06 -0800

    modifying nav for mobile

--- builds/dumbo/scripts/controllers/main.js
+++ builds/dumbo/scripts/controllers/main.js
@@ -10,4 +10,22 @@
 angular.module('dumboApp')
   	.controller('MainCtrl', function ($scope) {
   		console.log('MainCtrl');
+
+  		$(document).ready(function() {
+
+  			updateMenu();
+  			$(window).resize(function() {
+  				updateMenu();
+  			})
+
+  			function updateMenu() {
+  				if ($(window).width() <= 768) {
+  					console.log('inside if');
+  					$('#newPost').removeClass('btn btn-primary');
+  					$()
+  				}
+  			}
+
+  			
+  		});
   	});
