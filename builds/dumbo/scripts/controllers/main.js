88503637dd18502de379e76d0a4f52852d25e026
jclaw <jclaw3456@gmail.com>
2015-12-28 16:01:36 -0800

    working dropdown menu for mobile and tablets in nav

--- builds/dumbo/scripts/controllers/main.js
+++ builds/dumbo/scripts/controllers/main.js
@@ -19,21 +19,34 @@ angular.module('dumboApp')
   			})
 
   			function updateNav() {
-  				if ($(window).width() <= 768) {
+  				var b_small = 768,
+  					b_medium = 992;
+  				if ($(window).width() <= b_small) {
   					$('.navbar-right #newPostButton').removeClass('btn btn-primary');
   					if ($('.navbar-collapse .searchbar').length) {
-  						console.log('true');
   						$('.searchbar').appendTo('.navbar-header');
-  					} else {
-  						console.log('false');
   					}
   				} else {
+  					$('.navbar-right #newPostButton').addClass('btn btn-primary');
   					if (! $('.navbar-collapse .searchbar').length) {
-  						$('.searchbar').insertAfter('.navbarShop');
+  						$('.searchbar').insertAfter('.navbarMain');
   					}
   				}
-  			}
 
+  				if ($(window).width() <= 1051) {
+
+  					$('.navbarShop .dropdown-toggle').show();
+  					$('.navbarShop').addClass('dropdown');
+  					$('.navbarShop ul').removeClass('nav navbar-nav');
+  					$('.navbarShop ul').addClass('dropdown-menu');
+  				
+  				} else {
+   					$('.navbarShop .dropdown-toggle').hide();
+  					$('.navbarShop').removeClass('dropdown');
+  					$('.navbarShop ul').addClass('nav navbar-nav');
+  					$('.navbarShop ul').removeClass('dropdown-menu');
+  				}
+  			}
 
   		});
   	});
