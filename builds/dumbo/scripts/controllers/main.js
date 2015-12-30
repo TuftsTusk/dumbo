e15d36136481b2343ce260c048d8bd89eacf5fb0
jclaw <jclaw3456@gmail.com>
2015-12-30 13:01:11 -0800

    working nav

--- builds/dumbo/scripts/controllers/main.js
+++ builds/dumbo/scripts/controllers/main.js
@@ -11,42 +11,4 @@ angular.module('dumboApp')
   	.controller('MainCtrl', function ($scope) {
   		console.log('MainCtrl');
 
-  		$(document).ready(function() {
-
-  			updateNav();
-  			$(window).resize(function() {
-  				updateNav();
-  			})
-
-  			function updateNav() {
-  				var b_small = 768,
-  					b_medium = 992;
-  				if ($(window).width() <= b_small) {
-  					$('.navbar-right #newPostButton').removeClass('btn btn-primary');
-  					if ($('.navbar-collapse .searchbar').length) {
-  						$('.searchbar').appendTo('.navbar-header');
-  					}
-  				} else {
-  					$('.navbar-right #newPostButton').addClass('btn btn-primary');
-  					if (! $('.navbar-collapse .searchbar').length) {
-  						$('.searchbar').insertAfter('.navbarMain');
-  					}
-  				}
-
-  				if ($(window).width() <= 1051) {
-
-  					$('.navbarShop .dropdown-toggle').show();
-  					$('.navbarShop').addClass('dropdown');
-  					$('.navbarShop ul').removeClass('nav navbar-nav');
-  					$('.navbarShop ul').addClass('dropdown-menu');
-  				
-  				} else {
-   					$('.navbarShop .dropdown-toggle').hide();
-  					$('.navbarShop').removeClass('dropdown');
-  					$('.navbarShop ul').addClass('nav navbar-nav');
-  					$('.navbarShop ul').removeClass('dropdown-menu');
-  				}
-  			}
-
-  		});
   	});
