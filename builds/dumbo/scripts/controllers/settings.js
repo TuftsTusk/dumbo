c18ecb60d3ca61a806cc87d0e192128878475e37
jclaw <jclaw3456@gmail.com>
2016-02-21 14:12:43 -0500

    active nav link not working

--- builds/dumbo/scripts/controllers/settings.js
+++ builds/dumbo/scripts/controllers/settings.js
@@ -2,5 +2,16 @@
 
 angular.module('dumboApp')
   	.controller('SettingsCtrl', function ($scope) {
-		
+
+		$(document).ready(function() {
+			var settings = $('#settings');
+			var nav = settings.find('.settingsNav');
+			var links = nav.find('a');
+			console.log(links);
+			console.log(location.hash);
+
+			var curr = $('#settings').find('[href^="' + location.hash + '"]').addClass('active');
+			//links.not(curr).removeClass('active');
+		});
+
   	});
