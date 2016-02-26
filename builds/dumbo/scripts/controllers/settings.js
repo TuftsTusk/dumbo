eef270892036c8ca91e83f838ed7f8f4bb5fe4e5
jclaw <jclaw3456@gmail.com>
2016-02-26 14:30:42 -0500

    added template form

--- builds/dumbo/scripts/controllers/settings.js
+++ builds/dumbo/scripts/controllers/settings.js
@@ -2,14 +2,47 @@
 
 angular.module('dumboApp')
   	.controller('SettingsCtrl', ['$scope', '$routeParams', function ($scope, $routeParams) {
-		var $page = $routeParams.page;
+		var page = $routeParams.page;
+
+		var pages = {
+			'account': function() {
+				return {
+					title: 'Account settings',
+					subtitle: 'Your personal information. Only your first name is publicly visible.'
+				};
+			},
+			'addresses': function() {
+				return {
+					title: 'Addresses',
+				};
+			},
+			'notifications': function() {
+				return {
+					title: 'Notification settings',
+				};
+			},
+			'default': function() {
+				return {
+					title: page
+				};
+			}
+		}
+
+		if (pages[page]) {
+
+			$scope.page = pages[page]();
+		} else {
+			$scope.page = pages['default']();
+		}
+
+		//$scope.page = pages(page) ? pages(page) : pages(default);
 
 		updateSettingsNav();
 		$(window).on('hashchange', function() {
 			updateSettingsNav();
 		});
 
-		
+
 
 
 
