9b991e0d1317ec14a3c822bc9df60bb545a259f1
jclaw <jclaw3456@gmail.com>
2016-03-04 01:25:45 -0500

    basic data binding

--- builds/dumbo/scripts/controllers/userListings.js
+++ builds/dumbo/scripts/controllers/userListings.js
@@ -3,4 +3,49 @@
 angular.module('dumboApp')
   .controller('UserListingsCtrl', function ($scope) {
 
+	  $scope.title = 'Room 1';
+	  $scope.listingData = {
+		  address: '303 Boston Ave, Medford, MA',
+		  rooms: [
+			//   {
+			// 	  placeholder: $scope.title,
+			// 	  title: $scope.title,
+			// 	  rent: '',
+			// 	  dateAvailable: '',
+			// 	  dateUnavailable: '',
+			//   }
+		  ]
+	  }
+
+	  $scope.roomForm = {
+		  placeholder: $scope.title,
+		  title: $scope.title,
+		  rent: '',
+		  dateAvailable: '',
+		  dateUnavailable: '',
+		  DAFlexible: false,
+		  DUFlexible: false
+	  };
+
+	  $scope.checkEmpty = function(e, value) {
+		  //console.log(e, value);
+		  //console.log($scope.roomForm[value]);
+
+		  if (value == 'title') {
+			 if ($scope.roomForm[value] == '') {
+				 $scope.title = 'Room 1';
+			 } else {
+				 $scope.title = $scope.roomForm[value];
+			 }
+		  }
+
+		//   if (e.keyCode == 8 && $scope.listingData.rooms[0][value] == '') {
+		// 	  e.preventDefault();
+		// 	  console.log('empty');
+		// 	  //$scope.model[value] = '';
+		//   }
+	  };
+
+
+	  console.log($scope.listingData);
   });
