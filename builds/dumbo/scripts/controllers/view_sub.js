cf150de25a3a464b090f902f690e0f857ab2967f
Dan Callahan <dancallahan05@gmail.com>
2016-03-14 16:09:46 -0400

    maps work

--- builds/dumbo/scripts/controllers/view_sub.js
+++ builds/dumbo/scripts/controllers/view_sub.js
@@ -7,20 +7,84 @@
  * # ViewCtrl
  * Controller of the dumboApp
  */
+
+var lat;
+var lng;
+
+
+
 angular.module('dumboApp')
     .controller('SubCtrl', function ($scope, $http) {
-        console.log('SubCtrl');
         $http.get('../../get_listing.json').then(function (result) {
             $scope.listings = result.data;
-            $scope.source = result.data.image_gallery_link[0];
-            console.log(result.data);
 
+            lat = $scope.listings.lat;
+            lng = $scope.listings.lng;
+            
+            new google.maps.Marker({
+                position: {
+                    lat: parseFloat($scope.listings.lat),
+                    lng: parseFloat($scope.listings.lng)
+                },
+                map: map
+            });
+
+            initMap(42.4059385, -71.1197832);
         });
 
-
-    $scope.switch_source= function(array_place){
-        $scope.source = $scope.listings.image_gallery_link[array_place];
-    };
-
-
     });
+
+var map;
+
+function initMap(lat, lng) {
+    lat = parseFloat(lat);
+    lng = parseFloat(lng);
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: lat,
+            lng: lng
+        },
+        zoom: 15
+    });
+};
+
+$(document).ready(function () {
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+
+            map.panTo({
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            });
+
+
+            new google.maps.Marker({
+                position: {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                },
+                map: map
+            });
+
+
+        }, function (err) {
+            console.log(err);
+        });
+    } else {
+
+    }
+
+
+
+
+});
+//
+//google.maps.event.addDomListener(window, "load", function () {
+//    var myOptions = {
+//        zoom: 8,
+//        center: (42.4059385,-71.1197832),
+//    };
+//    var map = new google.maps.Map(document.getElementById("map"),
+//        myOptions);
+//});
