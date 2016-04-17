e0e80d6b6b58d0af73386f43eb3e6bfafd8abcf0
Dan Callahan <dancallahan05@gmail.com>
2016-04-17 12:15:27 -0400

    asdfajkhdf

--- builds/dumbo/scripts/app.js
+++ builds/dumbo/scripts/app.js
@@ -14,79 +14,7 @@ underscore.factory('_', function () {
 	return window._; //Underscore should be loaded on the page
 });
 
-angular
-<<<<<<< HEAD
-    .module('dumboApp', [
-    'ngAnimate',
-    'ngAria',
-    'ngCookies',
-    'ngMessages',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch',
-    'oitozero.ngSweetAlert',
-    'dumboApp.config',
-    'LocalStorageModule',
-    'underscore',
-	'ui.bootstrap'
-  ])
-    .config(function ($routeProvider, $httpProvider) {
-        $routeProvider
-            .when('/', {
-                templateUrl: 'views/home.html'
-            })
-            .when('/home', {
-                templateUrl: 'views/home.html'
-            })
-            .when('/login', {
-                templateUrl: 'views/login.html'
-            })
-            .when('/register', {
-                templateUrl: 'views/register.html'
-            })
-            .when('/newListing', {
-                templateUrl: 'views/newListing.html'
-            })
-            .when('/about', {
-                templateUrl: 'views/about.html'
-            })
-            .when('/listing', {
-                templateUrl: 'views/grid_view.html'
-            })
-      		.when('/mylistings/listing/:id' , {
-      				redirectTo: '/mylistings/listing/:id/general'
-      		})
-      		.when('/mylistings/listing/:id/:path' , {
-      				templateUrl: 'views/singleListing.html'
-      		})
-            .when('/listing/:id' , {
-                templateUrl: 'views/view_post.html'
-            })
-            .when('/settings' , {
-      		    redirectTo: '/settings/account'
-            })
-      		.when('/settings/:page' , {
-      		    templateUrl: 'views/settings.html'
-      		})
-            .when('/sublet' , {
-      		    templateUrl: 'views/view_sublet.html'
-      		})
-            .when('/terms' , {
-      		    templateUrl: 'views/terms.html'
-      		})
-            .when('/privacy' , {
-      		    templateUrl: 'views/privacy.html'
-      		})
-
-          .otherwise({
-              templateUrl: '404.html'
-          });
-          $httpProvider.defaults.withCredentials = true;
-          $httpProvider.interceptors.push('authInterceptor');
-      });
-=======
-.module('dumboApp', [
+angular.module('dumboApp', [
 	'ngAnimate',
 	'ngAria',
 	'ngCookies',
@@ -170,4 +98,4 @@ angular
 	$httpProvider.defaults.withCredentials = true;
 	$httpProvider.interceptors.push('authInterceptor');
 });
->>>>>>> master
+
