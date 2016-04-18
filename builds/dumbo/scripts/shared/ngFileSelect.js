afb0b40788c46578af15d3586159995c852c09e8
jclaw <jclaw3456@gmail.com>
2016-04-18 18:35:54 -0400

    loading image works, but not on multiple on one page

--- builds/dumbo/scripts/shared/ngFileSelect.js
+++ builds/dumbo/scripts/shared/ngFileSelect.js
@@ -4,14 +4,31 @@ angular.module('dumboApp')
     link: function($scope,el){
 
       el.bind("change", function(e){
-		var doneUploadingOneImage = function(url){
-			var obj = {'photo_url': url};
-			$scope.currentUploadTarget.push(obj);
-			$scope.save();
+		var doneUploadingOneImage = function(error, url, index){
+
+			if (error) {
+
+			}
+			else {
+				var obj = {'photo_url': url};
+				$scope.save();
+				// $scope.isUploading = false;
+				$scope.currentUploadTarget.push(obj);
+				console.log('done');
+			}
+
+			$scope.numFilesToUpload--;
+			if ($scope.numFilesToUpload == 0) $scope.uploading = false;
 		}
+
+
         var files = (e.srcElement || e.target).files;
+		$scope.numFilesToUpload = files.length;
+		$scope.numFilesTotal = files.length;
+		$scope.uploading = true;
 		$.each(files, function(index, file){
-			imageUploadService.uploadImage(file, doneUploadingOneImage);
+
+			imageUploadService.uploadImage(file, index, doneUploadingOneImage);
 		})
       })
     }
