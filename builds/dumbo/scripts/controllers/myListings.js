3b675faa43c247d851bef3976d005d593a3cb5d1
jclaw <jclaw3456@gmail.com>
2016-04-25 23:12:36 -0400

    drafts are read from localstorage

--- builds/dumbo/scripts/controllers/myListings.js
+++ builds/dumbo/scripts/controllers/myListings.js
@@ -1,6 +1,10 @@
 angular.module('dumboApp')
-  	.controller('MyListingsCtrl', function ($scope, listingDataService) {
-		listingDataService.getMeListing().then(
+.controller('MyListingsCtrl', function ($scope, listingDataService, localStorageService) {
+	$scope.drafts = [];
+	var localStorageKeys = [
+		'SubletListing'
+	];
+	listingDataService.getMeListing().then(
 		function success(res) {
 			$scope.listings = res.data;
 
@@ -8,5 +12,14 @@ angular.module('dumboApp')
 		},
 		function failure(res) {
 			console.log('fail');
-		});
-  	});
+		}
+	);
+
+	$.each(localStorageKeys, function(index, lsKey) {
+		var savedData = localStorageService.get(lsKey);
+		if (savedData) {
+			$scope.drafts.push(savedData.form);
+		}
+	});
+	console.log($scope.drafts);
+});
