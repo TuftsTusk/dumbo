8fc2d0924d202f7a3eb082eedebe65427ffd1bb1
Dan Callahan <dancallahan05@gmail.com>
2016-05-07 14:27:37 -0400

    styling

--- builds/dumbo/scripts/app.js
+++ builds/dumbo/scripts/app.js
@@ -41,7 +41,8 @@ var app = angular
 	'underscore',
 	'ui.bootstrap',
 	'angular-loading-bar',
-	'ngToast'
+	'ngToast',
+    'ngMaterial'
 ])
 .config(function ($routeProvider, $httpProvider) {
 	$routeProvider
