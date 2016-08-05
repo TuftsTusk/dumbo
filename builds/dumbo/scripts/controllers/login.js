1effee5417444e76d4d93c6cb260f8a65ca29999
BCefali <brian.cefali@gmail.com>
2016-08-05 13:39:47 -0400

    Sizable commit. Contents include: refactoring of login.js, updates to login.html to fix some annoying bugs including the one where the register form will try to log in if you hit the enter key despite the form clearly being the register form.

--- builds/dumbo/scripts/controllers/login.js
+++ builds/dumbo/scripts/controllers/login.js
@@ -1,168 +1,216 @@
-angular.module('dumboApp')
-.controller('LoginCtrl', function ($scope, userDataService, SweetAlert, userService,$routeParams, $location, ngToast) {
-    var id = $routeParams.id;
-    var key = $routeParams.key;
-    var action = $routeParams.action;
-    var loginType = $routeParams.loginType;
-    $scope.init = function() {
-        //setup page actions
-        if (loginType=="newuser"){
-            $scope.newuser = true;
-        } else {
-            $scope.newuser= false;
-        }
-        if (id && key && action=="confirm"){
-            userDataService.confirmAccount(id,key).then(function(data){
-                SweetAlert.swal({title: "Welcome!",
-                text: "Your account is confirmed. Please log in.",
-                timer: 2000,
-                showConfirmButton: false });
+angular.module('dumboApp').controller('LoginCtrl',
+    function ($scope, userDataService, SweetAlert, userService,$routeParams, $location, ngToast) {
+        var id = $routeParams.id;
+        var key = $routeParams.key;
+        var action = $routeParams.action;
+        var loginType = $routeParams.loginType;
 
-            }, function(data){
-                SweetAlert.swal({title: "Sorry",
-                text: "Please check your email, your account was not confirmed",
-                timer: 2000,
-                showConfirmButton: false });
-            })
-        }
-        if (id && key && action=="recover"){
-            $scope.resetPassword();
-        }
-    };
-
-    $scope.resetPassword = function() {
-        swal.withForm({  title: "Create a new password",
-        text: "Enter your new password:",
-        showCancelButton: true,
-        closeOnConfirm: false,
-        animation: "slide-from-top",
-        confirmButtonText: "Reset Password",
-        showLoaderOnConfirm: true,
-        formFields: [
-            { id: 'password', placeholder:'Password', type:'password' },
-            { id: 'confirmpass', placeholder:'Password confirmation', type:'password' }
-        ]
-    },
-    function(){
-        var password = this.swalForm.password;
-        var confirmpass = this.swalForm.confirmpass;
-        if (password === false || confirmpass === false) return false;
-        if (password === "") {
-            swal.showInputError("Please enter a password");
-            return false
-        }
-        if (confirmpass === "") {
-            swal.showInputError("Please confirm your password");
-            return false
-        }
-        if (password != confirmpass){
-            swal.showInputError("Password and confirmation do not match");
-            return false
-        }
-        swal("We're resetting your password", "Just a moment");
-        userDataService.changePassword(id, key, password, confirmpass).then(
-            function success(res){
-                swal("Your password has been reset", "Please login to continue" , "success");
-            },
-            function failure(res){
-                SweetAlert.swal("I'm sorry I can't do that", res.data.message, "error");
-            }
-
-        )
-    });
-};
-
-$scope.forgotPassword = function() {
-    swal({  title: "Forgot your password?",
-    text: "Enter your email address:",
-    type: "input",
-    showCancelButton: true,
-    closeOnConfirm: false,
-    animation: "slide-from-top",
-    confirmButtonText: "Reset Password",
-    inputPlaceholder: "me@tufts.edu",
-    showLoaderOnConfirm: true
-},
-function(email){
-    if (email === false) return false;
-    if (email === "") {
-        swal.showInputError("Please enter your email address");
-        return false
-    }
-    swal("We're resetting your password", "Just a moment");
-    userDataService.recoverPasswordByEmail(email).then(
-        function success(res){
-            swal("Please check your email", "We emailed you a link to reset your password" , "success");
-        },
-        function failure(res){
-            SweetAlert.swal("I'm sorry I can't do that", res.data.message, "error");
-        }
-
-    )
-});
-};
-
-$scope.login = function(){
-    $scope.dataLoading = true;
-    userDataService.login($scope.user).then(
-        function success(res){
-            userService.setLoggedIn($scope.user.email);
-            $scope.dataLoading = false;
-            $location.path('/');
-            ngToast.create({
-                className: 'success',
-                content: 'You have succesfully logged in',
-                timeout: 3000
-            });
-
-        },
-        function failure(res){
-            $scope.dataLoading = false;
-            console.log($scope.user);
-            if (res.status === -1 || !res.data || !res.data.message) {
-                SweetAlert.swal("Woops", "Looks like someone unplugged us. Please try again in a few.", "error");
+        $scope.init = function() {
+            //setup page actions
+            if (loginType=="newuser") {
+                $scope.newuser = true;
             } else {
-                if (res.data.type == 'USER_NOT_CONFIRMED_FAILURE'){
-                    swal({title: "Looks like your account isn't confirmed",
-                    text: "Click to resend confirmation email",
-                    type: "info",
-                    showCancelButton: true,
-                    closeOnConfirm: false,
-                    showLoaderOnConfirm: true, },
-                    function(){
-                        setTimeout(function(){
-                            userDataService.resendConfirmation($scope.user.email).then(
-                                function success(res){
-                                    swal("Please check your email to confirm your account.");
-                                },
-                                function failure(res){
-                                    SweetAlert.swal("Woops", "Looks like someone unplugged us. Please try again in a few.", "error");
-                                }
-                            )
+                $scope.newuser= false;
+            }
+            if (id && key && action=="confirm") {
+                userDataService.confirmAccount(id,key).then(function(data) {
+                    SweetAlert.swal({
+                        title: "Welcome!",
+                        text: "Your account is confirmed. Please log in.",
+                        timer: 2000,
+                        showConfirmButton: false
+                    });
+                }, function(data) {
+                    SweetAlert.swal({
+                        title: "Sorry",
+                        text: "Please check your email, your account was not confirmed",
+                        timer: 2000,
+                        showConfirmButton: false
+                    });
+                })
+            }
+            if (id && key && action=="recover") {
+                $scope.resetPassword();
+            }
+        };
 
-                        }, 2000); });
-                    } else {
+        $scope.resetPassword = function() {
+            swal.withForm({
+                title: "Create a new password",
+                text: "Enter your new password:",
+                showCancelButton: true,
+                closeOnConfirm: false,
+                animation: "slide-from-top",
+                confirmButtonText: "Reset Password",
+                showLoaderOnConfirm: true,
+                formFields: [
+                    { id: 'password', placeholder:'Password', type:'password' },
+                    { id: 'confirmpass', placeholder:'Password confirmation', type:'password' }
+                ]
+            },
+            function() {
+                var password = this.swalForm.password;
+                var confirmpass = this.swalForm.confirmpass;
+                if (password === false || confirmpass === false) return false;
+                if (password === "") {
+                    swal.showInputError("Please enter a password");
+                    return false;
+                }
+                if (confirmpass === "") {
+                    swal.showInputError("Please confirm your password");
+                    return false;
+                }
+                if (password != confirmpass) {
+                    swal.showInputError("Password and confirmation do not match");
+                    return false;
+                }
+                swal("We're resetting your password", "Just a moment");
+                userDataService.changePassword(id, key, password, confirmpass).then(
+                    function success(res) {
+                        swal("Your password has been reset", "Please login to continue" , "success");
+                    },
+                    function failure(res) {
                         SweetAlert.swal("I'm sorry I can't do that", res.data.message, "error");
                     }
-
-                }
+                );
             });
         };
-        $scope.register = function(){
+
+        $scope.forgotPassword = function() {
+            swal({
+                title: "Forgot your password?",
+                text: "Enter your email address:",
+                type: "input",
+                showCancelButton: true,
+                closeOnConfirm: false,
+                animation: "slide-from-top",
+                confirmButtonText: "Reset Password",
+                inputPlaceholder: "me@tufts.edu",
+                showLoaderOnConfirm: true
+            },
+            function(email) {
+                if (email === false) return false;
+                if (email === "") {
+                    swal.showInputError("Please enter your email address");
+                    return false
+                }
+                swal("We're resetting your password", "Just a moment");
+                userDataService.recoverPasswordByEmail(email).then(
+                    function success(res) {
+                        swal("Please check your email", "We emailed you a link to reset your password" , "success");
+                    },
+                    function failure(res) {
+                        SweetAlert.swal("I'm sorry I can't do that", res.data.message, "error");
+                    }
+                );
+            });
+        };
+
+        var login = function(){
             $scope.dataLoading = true;
-            userDataService.register($scope.user).then(
-                function success(res){
+            userDataService.login($scope.user).then(
+                function success(res) {
+                    userService.setLoggedIn($scope.user.email);
                     $scope.dataLoading = false;
                     $location.path('/');
-                    SweetAlert.swal("Welcome", "Check your email to verify your new account", "success");
+                    ngToast.create({
+                        className: 'success',
+                        content: 'You have succesfully logged in',
+                        timeout: 3000
+                    });
                 },
-                function failure(res){
+                function failure(res) {
+                    $scope.dataLoading = false;
+                    if (res.status === -1 || !res.data || !res.data.message) {
+                    SweetAlert.swal("Woops", "Looks like someone unplugged us. Please try again in a few.", "error");
+                    } else {
+                        if (res.data.type == 'USER_NOT_CONFIRMED_FAILURE') {
+                            swal({
+                                title: "Looks like your account isn't confirmed",
+                                text: "Click to resend confirmation email",
+                                type: "info",
+                                showCancelButton: true,
+                                closeOnConfirm: false,
+                                showLoaderOnConfirm: true
+                            }, function() {
+                                setTimeout(function() {
+                                    userDataService.resendConfirmation($scope.user.email).then(
+                                        function success(res){
+                                            swal("Please check your email to confirm your account.");
+                                        },
+                                        function failure(res){
+                                            SweetAlert.swal("Woops", "Looks like someone unplugged us. Please try again in a few.", "error");
+                                        }
+                                    );
+                                }, 2000);
+                            });
+                        } else {
+                            SweetAlert.swal("I'm sorry I can't do that", res.data.message, "error");
+                        }
+                    }
+                }
+            );
+        };
+
+        var register = function() {
+            $scope.dataLoading = true;
+            userDataService.register($scope.user).then(
+                function success(res) {
+                    $scope.dataLoading = false;
+                    $location.path('/');
+                    if (res.status == 200) {
+                        // A status of 200 implies we got a response body... this only happens when an email
+                        // was not sent as we are on a development server-204 is returned in that case
+                        var conf_url = userDataService._confirmAccountURL(res.data.id, res.data.key);
+                        SweetAlert.swal({
+                                title: "Proceed to Account Confirmation?",
+                                text: "This will confirm your new account.",
+                                type: "warning",
+                                showCancelButton: true,
+                                confirmButtonColor: "#DD6B55",
+                                confirmButtonText: "Yes, confirm it!",
+                                cancelButtonText: "No, don't confirm it!",
+                                closeOnConfirm: false,
+                                closeOnCancel: true
+                            },
+                            function(willConfirm) {
+                                if (willConfirm) {
+                                    userDataService.confirmAccount(res.data.id, res.data.key).then(
+                                        function success(conf_res) {
+                                            SweetAlert.swal("Confirmed", "The new account was successfully confirmed.", "success");
+                                        },
+                                        function failure(conf_res) {
+                                            SweetAlert.swal("Confirmation Failure", "The new account could not be confirmed.", "error");
+                                        }
+                                    );
+                                }
+                            });
+                    } else if (res.status == 204) {
+                        SweetAlert.swal("Welcome", "Check your email to verify your new account", "success");
+                    } else {
+                        SweetAlert.swal("Unexpected Status!", "Received an unknown status: " + res.status);
+                    }
+                },
+                function failure(res) {
                     $scope.dataLoading = false;
                     if (res.status === -1) {
                         SweetAlert.swal("Woops", "Looks like someone unplugged us. Please try again in a few.", "error");
                     } else {
                         SweetAlert.swal("One little thing", res.data.message, "error");
                     }
-                });
-            };
-        });
+                }
+            );
+        }
+
+        $scope.handleForm = function(context) {
+            if ($scope.newuser) {
+                //Assume register
+                register();
+            } else {
+                //Assume login
+                login();
+            }
+        }
+    }
+);
+
