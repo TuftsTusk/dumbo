732d96cf8103253732f3567033d5b515024df2f3
Michael Seltzer <michael.m.seltzer@gmail.com>
2016-08-16 11:10:54 -0400

    fix glyphicons for bower task

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,40 @@ sass = require('gulp-sass'),
 connect = require('gulp-connect'),
 http = require('http'),
 jasmine = require('gulp-jasmine-phantom'),
-gulpNgConfig = require('gulp-ng-config');
+gulpNgConfig = require('gulp-ng-config'),
+inject = require('gulp-inject'),
+concat = require('gulp-concat'),
+wiredep = require('wiredep'),
+filter = require('gulp-filter'),
+flatten = require('gulp-flatten');
+
+
+gulp.task('copy-bs-fonts', function(){
+  return gulp.src('bower_components/bootstrap/fonts/*.{eot,svg,ttf,woff,woff2}')
+    .pipe(gulp.dest('fonts/'));
+});
+
+// Only applies for fonts from bower dependencies
+// Custom fonts are handled by the "other" task
+gulp.task('fonts', ['copy-bs-fonts'], function () {
+  return gulp.src('./')
+    .pipe(filter('**/*.{eot,svg,ttf,woff,woff2}'))
+    .pipe(flatten())
+    .pipe(gulp.dest('/fonts/'));
+});
+
+gulp.task('bower', ['fonts'], injectBower);
+
+function injectBower() {
+    var target = gulp.src('./index.html');
+    var js = gulp.src(wiredep().js);
+    var css = gulp.src(wiredep().css);
+
+    return target
+        .pipe(inject(js.pipe(concat('bower.js')).pipe(gulp.dest('./scripts'))))
+        .pipe(inject(css.pipe(concat('bower.css')).pipe(gulp.dest('./styles'))))
+        .pipe(gulp.dest('./'));
+}
 
 
 
@@ -25,7 +58,7 @@ gulp.task('productionEnv', function () {
     .pipe(gulpNgConfig('dumboApp.config', {
         environment: 'production'
     }))
-    .pipe(gulp.dest('/scripts/'))
+    .pipe(gulp.dest('scripts/'))
 });
 
 gulp.task('stagingEnv', function () {
@@ -82,15 +115,15 @@ gulp.task('serve', ['express'], function() {
     });
 });
 
-gulp.task('production',['productionEnv', 'sass'], function(){
+gulp.task('production',['productionEnv', 'bower', 'sass'], function(){
     var port = process.env.PORT || 8080;
     var express = require('express');
     var app = express();
-    app.use(express.static('welcome'));
+    app.use(express.static('./'));
     app.listen(port);
 });
 
-gulp.task('staging',['stagingEnv', 'sass'], function(){
+gulp.task('staging',['stagingEnv', 'bower', 'sass'], function(){
     var port = process.env.PORT || 8080;
     var express = require('express');
     var app = express();
@@ -99,4 +132,4 @@ gulp.task('staging',['stagingEnv', 'sass'], function(){
 
 });
 
-gulp.task('default', ['sass', 'watch', 'serve', 'developmentEnv']);
+gulp.task('default', ['sass', 'bower', 'watch', 'serve', 'developmentEnv']);
