a2b4fb129252979d958906623cb4b45e7eb39587
Michael Seltzer <michael.m.seltzer@gmail.com>
2016-08-17 01:39:51 -0400

    Multilisting Design iteration (#61)
    * refactor file locations to root
    
    * add back original script tags and style sheets
    
    * fix glyphicons for bower task
    
    * auto inject vendor and app dependency in correct order
    
    * automate adding vendor and app dependencies
    
    * update travis ci node version
    
    * dev dependecy cleanup
    
    * fix dependency for travis
    
    * fix dependency for travis
    
    * fix dependency for travis
    
    * fix dependency for travis
    
    * append uid to generated files and cleanup for caching
    
    * new layout for multilisting with better layout
    
    * more margin for price label
    
    * change animation

--- scripts/app.js
+++ scripts/app.js
@@ -42,7 +42,8 @@ var app = angular
 	'ui.bootstrap',
 	'angular-loading-bar',
 	'ngToast',
-	'ngMaterial'
+	'ngMaterial',
+	'infinite-scroll'
 ])
 .config(function ($routeProvider, $httpProvider) {
 	$routeProvider
@@ -151,12 +152,23 @@ var app = angular
 			loggedIn: requireLogin
 		}
 	})
+	.when('/gridlist' , {
+		templateUrl: 'views/gridList.html',
+		resolve: {
+			loggedIn: requireLogin
+		}
+	})
 	.otherwise({
 		templateUrl: '404.html'
 	});
 	$httpProvider.defaults.withCredentials = true;
 	$httpProvider.interceptors.push('authInterceptor');
 });
+
+
+app.constant("LISTING", {
+	"MAXSUMMARYLENGTH": "200"
+})
 
 
 app.config(['ngToastProvider', function(ngToast) {
--- scripts/controllers/multiLayout.js
+++ scripts/controllers/multiLayout.js
@@ -1,28 +1,25 @@
 'use strict';
 
 angular.module('dumboApp')
-	.controller('multiLayout', function ($scope, listingDataService, $route) {
-        $scope.listings = {};
-        $scope.listings.error = true;
-		$scope.listingType = $route.current.$$route.listingType;
+.controller('multiLayout', function ($scope, listingDataService, $route, LISTING, $location) {
+	$scope.listings = {};
+	$scope.listings.error = true;
+	$scope.listingType = $route.current.$$route.listingType;
+	$scope.LISTING = LISTING;
 
-        $scope.init = function(){
-            listingDataService.getListings()
-            .then(function success(request){
-                console.log(request);
-                $scope.listings = request.data;
-                $scope.listings.error = false;
 
-                console.log($scope.listings);
-            }, function failure(request){
-                $scope.listings.error = true;
-            })
-        }
+	$scope.init = function(){
+		listingDataService.getListings()
+		.then(function success(request){
+			$scope.listings = request.data;
+			$scope.listings.error = false;
+		}, function failure(request){
+			$scope.listings.error = true;
+		})
+	};
 
-	})
-    .config(function($mdThemingProvider) {
-  $mdThemingProvider.theme('dark-grey').backgroundPalette('grey').dark();
-  $mdThemingProvider.theme('dark-orange').backgroundPalette('orange').dark();
-  $mdThemingProvider.theme('dark-purple').backgroundPalette('deep-purple').dark();
-  $mdThemingProvider.theme('dark-blue').backgroundPalette('blue').dark();
-});
+	$scope.viewListing = function(uid){
+		$location.path('/listing/' + uid);
+	};
+
+})
