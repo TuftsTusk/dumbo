ed46564f49483e5d7cac0ddd99b6265bf22fd1f2
jclaw <jclaw3456@gmail.com>
2016-08-25 10:15:37 -0700

    added comments to Page

--- scripts/shared/Page.js
+++ scripts/shared/Page.js
@@ -1,3 +1,7 @@
+ // Get the page title by Page.title()
+ // Set the page title by Page.setTitle('string')
+    // by default, setTitle will append ' | Tusk' to the page title. set
+    // addTusk to false if you don't want that, e.g. Page.setTitle('string', false)
 angular.module('dumboApp')
 .factory('Page', function(){
     $('head #defaultTitle').hide();
