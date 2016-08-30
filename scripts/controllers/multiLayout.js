bb813df662ec4915d2a8021ea8d59d7e7eb4e0c1
Michael Seltzer <michael.m.seltzer@gmail.com>
2016-08-30 12:58:57 -0400

    map view with labels and click to scrollå (#73)

--- scripts/controllers/multiLayout.js
+++ scripts/controllers/multiLayout.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('dumboApp')
-.controller('multiLayout', function ($scope, listingDataService, listingMap, $route, LISTING, $location, ngToast, $routeParams) {
+.controller('multiLayout', function ($scope, listingDataService, listingMap, $route, LISTING, $location, $anchorScroll, ngToast, $routeParams, uiGmapGoogleMapApi, uiGmapIsReady, $timeout) {
 	$scope.listings = {};
 	$scope.listings.error = true;
 	$scope.listingType = $route.current.$$route.listingType || $routeParams.listingType;
@@ -10,25 +10,75 @@ angular.module('dumboApp')
 	$scope.searchTerm = $routeParams.searchTerm;
 	$scope.myListings = $route.current.$$route.myListings;
 
-
 	$scope.init = function(){
-		if ($scope.myListings){
-			listingDataService.getMeListing()
-			.then(function success(request){
-				$scope.listings = request.data;
-				$scope.listings.error = false;
-			}, function failure(request){
-				$scope.listings.error = true;
-			})
-		} else {
-			listingDataService.getListingsByType($scope.listingType, $scope.searchTerm)
-			.then(function success(request){
-				$scope.listings = request.data;
-				$scope.listings.error = false;
-			}, function failure(request){
-				$scope.listings.error = true;
-			})
-		}
+		$scope.mapLoaded = false;
+		$scope.map = {
+			center: {
+				latitude: 42.4074843,
+				longitude: -71.1190232
+			},
+			zoom: 12,
+			markers: [],
+			markersEvents: {
+				click: function(marker, eventName, listing) {
+					$scope.map.center = listing;
+					$scope.map.window.listing = listing;
+					$scope.map.window.show = true;
+					$("#" + listing.id)[0].scrollIntoView({
+						behavior: "smooth",
+						block: "start"
+					});
+					$scope.selectedId = listing.id;
+				}
+			},
+			window: {
+				marker: {},
+				show: false,
+				closeClick: function() {
+					this.show = false;
+				},
+				options: {}
+			}
+		};
+
+		uiGmapGoogleMapApi.then(function(maps) {
+			$timeout(function(){
+				$scope.mapLoaded = true;
+			}, 100);
+			$scope.maps = maps;
+			if ($scope.myListings){
+				listingDataService.getMeListing()
+				.then(function success(request){
+					$scope.listings = request.data;
+					$scope.listings.error = false;
+				}, function failure(request){
+					$scope.listings.error = true;
+				})
+			} else {
+				listingDataService.getListingsByType($scope.listingType, $scope.searchTerm)
+				.then(function success(request){
+					$scope.listings = request.data;
+					$scope.listings.error = false;
+
+					//add markers
+					if ($scope.listingType == 'SubletListing'){
+						for (var i=0; i < $scope.listings.length; i++){
+							console.log($scope.listings[i]);
+							var ret = {
+								id:$scope.listings[i].listing._id,
+								latitude: $scope.listings[i].listing.apt_info.lat,
+								longitude: $scope.listings[i].listing.apt_info.lng,
+								listing:$scope.listings[i].listing
+							};
+							$scope.map.markers.push(ret);
+						}
+					}
+
+				}, function failure(request){
+					$scope.listings.error = true;
+				})
+			}
+		});
 
 		//update UI for searching
 		$scope.searchInput = $scope.searchTerm;
@@ -36,7 +86,7 @@ angular.module('dumboApp')
 
 	$scope.viewListing = function(uid){
 		if (uid == undefined || uid ==null){
-			///TODO: Send Goolge Analytics event for this failure
+			///TODO: Send Google Analytics event for this failure
 			ngToast.create({
 				className: 'warning',
 				content: "Post is currently unavailable. Please try again later.",
